fb2efdeb84c729e7760aa6048b899e05bfa78aee
Julián Stejman <jstejman@gmail.com>
2025-11-01 15:07:58 -0300

    channel.py terminado + tests

--- src/channel.py
+++ src/channel.py
@@ -13,8 +13,23 @@ class Channel:
     def encode(self, sym: np.ndarray, reporter: Reporter) -> np.ndarray:
         reporter.append_line("Canal", BLUE, "Aplicando AWGN/atenuación")
 
-        # TODO: aplicar_canal (AWGN)
-        return sym
+        num_sym = sym.shape[0] if sym.ndim >1 else 1
+        rng = self.rng if self.rng is not None else np.random.default_rng()
+        if self.with_fading:
+            fades = rng.uniform(0.5, 0.9, size=num_sym)
+            # aplicar la atenuación a los símbolos
+            fades_reshaped = fades.reshape(-1, *(1,) * (sym.ndim - 1))
+            sym = sym * fades_reshaped
+            reporter.append_line("Canal", BLUE, f"Atenuación aplicada (min={fades.min()}, max = {fades.max()}) ")
+        else:
+            fades = None
+        # se calcula sigma de ruido desde el Eb/N0 asumiendo Eb = 1
+        ebn0_lin = 10**(self.eb_n0_db/10)
+        N0 = 1/ebn0_lin
+        sigma = np.sqrt(N0/2)
+        noise = rng.normal(0,sigma,sym.shape)
+        reporter.append_line("Canal", BLUE, f"Eb/N0={self.eb_n0_db} dB -> sigma = {sigma}")
+        return sym + noise 
 
     def decode(self, sym: np.ndarray) -> np.ndarray:
         return sym
